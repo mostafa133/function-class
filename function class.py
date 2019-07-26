43cf25c4e18094d47d8fe8cc4b940b757997b349
mostafa133 <51473625+mostafa133@users.noreply.github.com>
2019-07-26 12:21:59 +0300

    Create function class.py

---
+++ function class.py
@@ -0,0 +1,14 @@
+
+class dog:
+    def __init__(self):
+        print("new dog born...")
+    def bark(self):
+        print("dog is barking")
+rix = dog()
+rafi = dog()
+print(type(rix))
+print(type(rafi))
+
+rix.bark()
+
+
